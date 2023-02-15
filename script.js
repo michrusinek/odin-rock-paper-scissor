c875aa39a93520898b09556a578d300997ad4ef5
michrusinek <mich.rusinek@gmail.com>
2023-02-15 17:33:19 +0100

    Added functionality that checks if the game is over

--- script.js
+++ script.js
@@ -21,47 +21,112 @@ function checkIfScissors(playerSelection) {
     return (playerSelection == "scissors" ? "" : "s"); 
 }
 
+function countScore(playerWin = false, computerWin = false, playerScore, computerScore) {
+    if (playerWin) {
+        playerScore++;
+    } else if (computerWin) {
+        computerScore++;
+    }
+    return {player: playerScore, computer: computerScore};
+}
+
+function checkIfGameOver(playerScore, computerScore) {
+    if (playerScore === 5) {
+        return{
+            msg:"You won the round!",
+            isOver: true
+        };
+    } else if (computerScore === 5) {
+        return {
+            msg:"You lost the round!",
+            isOver: true
+        };
+    } else {
+        return {isOver: false};
+    }
+}
+
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === "paper" && computerSelection === "scissors") {
         printChoices(playerSelection, computerSelection)
-        console.log('You lose! Scissors beat paper.');
-        return 'You lose! Scissors beat paper.';
+        return {
+            msg:'You lose! Scissors beat paper.',
+            playerWin: false,
+            computerWin: true
+        }
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         printChoices(playerSelection, computerSelection)
-        console.log('You lose! Paper beats rock.');
-        return 'You lose! Paper beats rock.'
+        return {
+            msg:'You lose! Paper beats rock.',
+            playerWin: false,
+            computerWin: true
+        }
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         printChoices(playerSelection, computerSelection)
-        console.log("You lose! Rock beats scissors");
-        return "You lose! Rock beats scissors";
+        return {
+            msg:"You lose! Rock beats scissors",
+            playerWin: false,
+            computerWin: true
+        }
     } else if (playerSelection === computerSelection) {
         printChoices(playerSelection, computerSelection)
-        console.log("Draw");
-        return "Draw"
+        return {
+            msg:"Draw",
+            playerWin: false,
+            computerWin: false
+        }
     } else {
         printChoices(playerSelection, computerSelection)
-        console.log(`You win! ${capitalize(playerSelection)} beat${checkIfScissors(playerSelection)} ${computerSelection}`);
-        return `You win! ${capitalize(playerSelection)} beat${checkIfScissors(playerSelection)} ${computerSelection}`;
+        return {
+            msg:`You win! ${capitalize(playerSelection)} beat${checkIfScissors(playerSelection)} ${computerSelection}`,
+            playerWin: true,
+            computerWin: false
+        }
     }
 
 }
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         const playerSelection = prompt("Rock, paper or scissors:");
-//         const computerSelection = getComputerChoice();
-//         playRound(playerSelection, computerSelection);
-//     }
-// }
-// game();
-
 const buttons = document.querySelectorAll('button');
 const scoreContainer = document.querySelector('#score');
+let playerScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        scoreContainer.textContent = playRound(button.textContent, getComputerChoice());
+    button.addEventListener('click', function() {
+        result = playRound(button.textContent, getComputerChoice())
+
+        playerScore = countScore(
+            result.playerWin, result.computerWin,
+            playerScore, computerScore
+            ).player;
+
+        computerScore = countScore(
+            result.playerWin, result.computerWin,
+            playerScore, computerScore
+            ).computer;
+
+        if (checkIfGameOver(playerScore, computerScore).isOver) {
+            console.log("Game over");
+            scoreContainer.textContent = 
+            checkIfGameOver(playerScore, computerScore).msg
+            +
+            `\nScore\nPlayer: ${playerScore}
+            \nComputer: ${computerScore}`;
+        } else {
+            scoreContainer.textContent = 
+            result.msg
+            +
+            `\nScore\nPlayer: ${playerScore}
+            \nComputer: ${computerScore}`;
+        }
+        // scoreContainer.textContent = 
+        // result.msg
+        // +
+        // `\nScore\nPlayer: ${playerScore}
+        // \nComputer: ${computerScore}`;
+        // console.log(checkIfGameOver(playerScore, computerScore));
     })
 })
